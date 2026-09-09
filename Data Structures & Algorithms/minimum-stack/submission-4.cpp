2f1a888eb48097c3e26bdc52de7962c9ebb730e9
B.P <barsharani.padhy13@gmail.com>
2026-09-09 12:48:20 +0530

    Add: minimum-stack - submission-4

---
+++ Data Structures & Algorithms/minimum-stack/submission-4.cpp
@@ -0,0 +1,38 @@
+class MinStack {
+public:
+
+    stack<int> minst;
+    stack<int> st;
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        st.push(val);
+
+        if(minst.empty() || val <= minst.top()){
+            minst.push(val);
+        }
+        
+    }
+    
+    void pop() {
+      
+      
+       if(minst.top() == st.top()){
+        minst.pop();
+       }
+       st.pop();
+        
+    }
+    
+    int top() {
+        return st.top();
+        
+    }
+    
+    int getMin() {
+        return minst.top();
+        
+    }
+};
